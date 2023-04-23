8c38fe1c2208793830680fba00ff1d46d8873ea3
Salvatore Lorenzo Renne <63696028+slrenne@users.noreply.github.com>
2023-04-23 19:56:20 +0200

    Update sim.R

--- scripts/sim.R
+++ scripts/sim.R
@@ -1,2 +1,10 @@
 # set seed
-set
+set.seed(230423)
+
+# let's simulate the topics
+# some topics  will be more covered
+# in the trainig set of chatGTP, 
+# we hence expect it to be important 
+
+Tmax <- 10
+
