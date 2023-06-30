9ee8067990830098ebd95260972649f9ba7a59b4
RENNE Salvatore HUNIMED <srenne@hunimed.eu>
2023-06-30 16:48:04 +0200

    stan model

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -20,7 +20,9 @@ dat<- list(
   S = rep(A$S, 4),  # scenario ID
   Pr = rep(Pr, 4), # Prompt type
   l_f = length(unique(A$f)),
-  l_Pr = length(unique(Pr))
+  l_Pr = length(unique(Pr)),
+  l_A = length(U),
+  l_S = length(A$S)
 )
 
 m.u <- ulam(
@@ -57,94 +59,9 @@ m.u <- ulam(
     
     ) , data = dat, chains = 4, cores = 8, cmdstan = TRUE, threads = 2) 
 
-
-
-m.e <- ulam(
-  alist(
-    E ~ dpois(lambda),
-    log(lambda) <- -1 *alpha_e_bar + 
-    zeta_u[A]*sigma_u_a + 
-    beta_e[P,f] + 
-    gamma_u[Pr]*sigma_u_Pr + 
-    delta_u[S]*sigma_u_S +
-    epsilon_u[f]*sigma_u_f,
-    
-    # adaptive priors - non-centered
-    transpars> matrix[P,l_f]:beta_e <-
-      compose_noncentered( sigma_e_P , L_Rho_e_P , z_e_P ),
-    matrix[l_f,P]:z_e_P ~ normal( 0 , 1 ),    
-    
-    # fixed priors
-    alpha_e_bar ~ normal(0,1.5),
-    zeta_u[A] ~ normal(0,1.5),
-    gamma_u[Pr] ~ normal(0,1.5),
-    delta_u[S] ~ normal(0,1.5),
-    epsilon_u[f] ~ normal(0,1.5),
-    sigma_u_a ~ dexp(1),
-    sigma_u_Pr ~ dexp(1),
-    sigma_u_S ~ dexp(1),
-    sigma_u_f ~ dexp(1),
-    vector[l_f]:sigma_e_P ~ dexp(1),
-    cholesky_factor_corr[l_f]:L_Rho_e_P ~ lkj_corr_cholesky( 2 ),
-    
-    # computing ordinary correlation matrix from Cholesky factors
-    gq> matrix[l_f,l_f]:Rho_e_P <<- multiply_lower_tri_self_transpose(L_Rho_e_P)
-    
-    ) , data = dat, chains = 4, cores = 8, cmdstan = TRUE, threads = 2) 
-
-
-m <- ulam(
-  alist(
-    
-    # model of usefulness
-    U ~ bernoulli(p), 
-    
-    # model of error counts
-    E ~ dpois(lambda),
-    
-    # model of usefulness
-    logit(p) <- 
-      alpha_u_bar + 
-      zeta_u[A]*sigma_u_a + 
-      beta_u[P,f] + 
-      gamma_u[Pr]*sigma_u_Pr + 
-      delta_u[S]*sigma_u_S +
-      epsilon_u[f]*sigma_u_f,
-    
-    # model of error counts
-    log(lambda) <- -1 *alpha_e_bar + 
-      zeta_u[A]*sigma_u_a + 
-      beta_e[P,f] + 
-      gamma_u[Pr]*sigma_u_Pr + 
-      delta_u[S]*sigma_u_S +
-      epsilon_u[f]*sigma_u_f,
-    
-    # adaptive priors - non-centered
-    transpars> matrix[P,l_f]:beta_u <-
-      compose_noncentered( sigma_u_P , L_Rho_u_P , z_u_P ),
-    matrix[l_f,P]:z_u_P ~ normal( 0 , 1 ),  
-    transpars> matrix[P,l_f]:beta_e <-
-      compose_noncentered( sigma_e_P , L_Rho_e_P , z_e_P ),
-    matrix[l_f,P]:z_e_P ~ normal( 0 , 1 ),   
-    
-    # fixed priors
-    alpha_e_bar ~ normal(0,1.5),
-    alpha_u_bar ~ normal(0,1.5),
-    zeta_u[A] ~ normal(0,1.5),
-    gamma_u[Pr] ~ normal(0,1.5),
-    delta_u[S] ~ normal(0,1.5),
-    epsilon_u[f] ~ normal(0,1.5),
-    sigma_u_a ~ dexp(1),
-    sigma_u_Pr ~ dexp(1),
-    sigma_u_S ~ dexp(1),
-    sigma_u_f ~ dexp(1),
-    vector[l_f]:sigma_u_P ~ dexp(1),
-    cholesky_factor_corr[l_f]:L_Rho_u_P ~ lkj_corr_cholesky( 2 ),
-    vector[l_f]:sigma_e_P ~ dexp(1),
-    cholesky_factor_corr[l_f]:L_Rho_e_P ~ lkj_corr_cholesky( 2 ),
-    
-    # computing ordinary correlation matrix from Cholesky factors
-    gq> matrix[l_f,l_f]:Rho_u_P <<- multiply_lower_tri_self_transpose(L_Rho_u_P),
-    gq> matrix[l_f,l_f]:Rho_e_P <<- multiply_lower_tri_self_transpose(L_Rho_e_P)
-    
-  ) , data = dat, chains = 4, cores = 8, cmdstan = TRUE, threads = 2) 
+# model written in stan 
+m <- cstan( file = 'scripts/mU_latA.stan', 
+            data = dat, 
+            chains = 4, 
+            cores = 4, 
+            iter = 1000)
---
+++ scripts/mU_latA.stan
@@ -0,0 +1,60 @@
+// 
+data{
+    int l_A; // length of the dataset
+    int l_Pr; // length of the prompts
+    int l_f; // length of the fiels
+    int l_S; // length of the scenarios
+    int P[l_A]; // Pathologist ID in answers
+    int A[l_A]; // answer ID in answers
+    int E[l_A]; // Error count ID in answers
+    int U[l_A]; // usefulness evaluation ID in answers
+    int S[l_A]; // scenario ID in answers
+    int Pr[l_A]; // prompts id in answers
+    int f[l_A];  // field ID in answers
+}
+parameters{
+    real alpha_u;
+    real alpha_e;
+    vector[l_Pr] gamma;
+    vector[l_S] delta;
+    vector[l_f] beta_K;
+    real<lower=0> sigma_K;
+    real<lower=0> sigma_Pr;
+    real<lower=0> sigma_S;
+    matrix[l_f,4] z_u_P;
+    vector[l_A] zeta_u;
+    vector<lower=0>[l_f] sigma_u_P;
+    cholesky_factor_corr[l_f] L_Rho_u_P;
+}
+transformed parameters{
+    vector[l_A] mu;
+    mu =  beta_K[f]*sigma_K + gamma[Pr]*sigma_Pr + delta[S]*sigma_S;
+    matrix[4,l_f] beta_PF;
+    vector[l_A] p;
+    beta_PF = (diag_pre_multiply(sigma_u_P, L_Rho_u_P) * z_u_P)';
+    for(i in 1:l_A) {
+      p[i] = alpha_u + mu[i] + beta_PF[P[i], f[i]];
+    }
+    vector[l_A] lambda;
+    for(i in 1:l_A) {
+      lambda[i] = - 1 * alpha_e + mu[i] + beta_PF[P[i], f[i]];
+    }
+}
+model{
+    L_Rho_u_P ~ lkj_corr_cholesky( 2 );
+    sigma_u_P ~ exponential( 0.5 );
+    sigma_S ~ exponential( 0.5 );
+    sigma_Pr ~ exponential( 0.5 );
+    sigma_K ~ exponential( 0.5 );
+    beta_K ~ normal( 0 , 0.5 );
+    delta ~ normal( 0 , 0.5 );
+    gamma ~ normal( 0 , 0.5 );
+    alpha_u ~ normal( 0 , 0.5 );  
+    alpha_e ~ normal( 0 , 0.5 );
+    U ~ bernoulli_logit( p );
+    E ~ poisson_log( lambda );
+}
+generated quantities{
+  matrix[l_f,l_f] Rho_u_P;
+  Rho_u_P =  multiply_lower_tri_self_transpose(L_Rho_u_P);    
+}
