3e0e1f2a0e047a8054e952d2d2353290d2511b39
Ömer Mintemur <omermintemur@gmail.com>
2024-02-16 12:02:28 +0300

    Add files via upload

--- scripts/real_data_analysis.R
+++ scripts/real_data_analysis.R
@@ -1,36 +1,31 @@
-# set seed
-set.seed(231003)
+
 
 #data import
-library(googlesheets4)
-gs4_deauth() # #avoid GS4 to ask you to log in in google
-url.db <- 'https://docs.google.com/spreadsheets/d/1L_DDxAoDRu1EYoPncjdbXfJmCSGcs-FESRuMs6i4rBo/edit?usp=sharing'
-db <- read_sheet(url.db)
+#library(googlesheets4)
+#gs4_deauth() # #avoid GS4 to ask you to log in in google
+#url.db <- 'https://docs.google.com/spreadsheets/d/1L_DDxAoDRu1EYoPncjdbXfJmCSGcs-FESRuMs6i4rBo/edit?usp=sharing'
+#db <- read_sheet(url.db)
 
-str(db)
+library(vioplot)
+# set seed
+set.seed(231003)
+library(rethinking)
 
-# mean usefulness
-mean(as.integer(factor(db$`was the advice useful?`))-1)
-# number of errors 
-mean(db$`Total number of error`)
-round(proportions(table(db$`Total number of error`)),2)
-propo
-frequency()
-hist(db$`Total number of error`)
-hist
+db <- read.csv('PathGPT_Evaluation_Final_V1.csv',sep=";")
+
 
 # let's start from the model of the
 dat<- list(
-  E = db$`Total number of error`,       # number of errors
+  E = db$Total.number.of.error,       # number of errors
   U = as.integer(
-    as.factor(db$`was the advice useful?`)) - 1 ,       # usefulness
-  A = db$`Question ID`,  # ID Answers
+    as.factor(db$was.the.advice.useful.)) - 1 ,       # usefulness
+  A = db$Question.ID,  # ID Answers
   P = as.integer(
-    as.factor(db$`Email Address`)),  # ID pathologist
+    as.factor(db$Username)),  # ID pathologist
   f = as.integer(
     as.factor(db$Area)),  # field ID  
   S = as.integer(
-    as.factor(db$`Scenario ID`)),  # scenario ID
+    as.factor(db$Scenario.ID)),  # scenario ID
   Pr = as.integer(
     as.factor(
       paste(db$type,db$reference))))  # Prompt type
@@ -42,30 +37,25 @@ dat$l_a <- length(unique(dat$A))
 dat$l_Pat <- length(unique(dat$P))
 
 
+
 # model written in stan 
-m <- cstan( file = 'scripts/mU_latA.stan', 
+m <- cstan( file = 'mU_latA.stan', 
             data = dat, 
             chains = 4, 
             cores = 4, 
             iter = 2000)
 
-dashboard(m)
-precis(m)
-precis(m,2,pars = 'sigma_u_P')
+#dashboard(m)
+#precis(m)
+#precis(m,2,pars = 'sigma_u_P')
 par(mfrow = c ( 1, 1)) # resetting graphical parameters
-plot(precis(m))
-plot(precis(m,2,pars = 'sigma_u_P'))
+#plot(precis(m))
+#plot(precis(m,2,pars = 'sigma_u_P'))
 
 post <- extract.samples(m) # extract the posterior
 
-plot(precis(m, 3, par = 'beta_PF'))
+#plot(precis(m, 3, par = 'beta_PF'))
 
-# 
-# U_link <- function( f, Pr, S){
-#   U <- with(post, {
-#     beta_K[ , f]*sigma_K })
-#   return(mean(sim_A))
-# }
 
 A_link <- function( f, Pr, S){
   sim_A <- with(post, {
@@ -81,10 +71,22 @@ sim_p <- inv_logit(c(sim_p))
 l <- length(sim_p)
 sim_U <- rbern(l,sim_p)
 HPDI(sim_p)
+mean(sim_p)
+png(filename="PlotFull.png", width=4000, height=3500, bg="white",res=300)
+par(mfrow = c(2, 2),mar=c(8,6,4,3)+.1)
 
-png('./figures/usefulness.png', 
-    width = pic.size, height = pic.size, res = 300)
-plot(N, ylim = c(- 0.1,2.5), xlim = c(0,1),
+
+line = 1
+cex = 2
+side = 3
+adj=-0.05
+
+#png('./figures/usefulness.png', 
+#    width = pic.size, height = pic.size, res = 300)
+
+# FIRST PLOT -------------------------------------------------
+
+plot(NULL, ylim = c(- 0.1,2.5), xlim = c(0,1),
      xlab = 'Probability', ylab = 'Density', 
      main = 'Usefulness')
 abline(v = 0.5, lty = 2)
@@ -93,20 +95,26 @@ useful <- data.frame(proportions(table(sim_U)))
 useful[,2] <- useful[,2] * 2.5
 for(i in 1:2) lines(i-1, useful[i,2], lwd=6, type = 'h')
 axis(4, at = c(0,1.25,2.5), labels = c("0","0.5", "1"))
-dev.off()
+mtext("A", side=side, line=line, cex=cex, adj=adj)
+
+# FIRST PLOT -------------------------------------------------
 
 ### to check 
 
+l <- length(dat$E)
 sim_lambda <- matrix(nrow = 4000, ncol = l)
 for(i in 1:l ) sim_lambda[,i] <- post$alpha_e - simA[,i]
 sim_lambda <- exp(c(sim_lambda))
 l <- length(sim_lambda)
 sim_E <- rpois(l,sim_lambda)
 HPDI(sim_lambda)
+mean(sim_lambda)
 
-png('./figures/accuracy.png', 
-    width = pic.size, height = pic.size, res = 300)
-plot(N, ylim = c(0 ,0.5), xlim = c(0,20),
+
+#png('./figures/accuracy.png', 
+#    width = pic.size, height = pic.size, res = 300)
+# SECOND PLOT -------------------------------------------------
+plot(NULL, ylim = c(0 ,0.5), xlim = c(0,20),
      xlab = 'Probability', ylab = 'Density', 
      main = 'Accuracy')
 dens(sim_lambda, lwd = 3, add = TRUE, adj = 1, col = 'red')
@@ -114,67 +122,85 @@ errors <- data.frame(proportions(table(sim_E)))
 errors[,2] <- errors[,2] * 0.5
 for(i in 1:21) lines(i-1, errors[i,2], lwd=6, type = 'h')
 axis(4, at = c(0,0.25,.5), labels = c("0","0.5", "1"))
-dev.off()
+mtext("B", side=side, line=line, cex=cex, adj=adj)
+# SECOND PLOT -------------------------------------------------
 
 
+# THIRD PLOT ----------------------------------------------------
+
+col <- c("#332288","#AA4499","#117733","#661100","#88CCEE","#DDCC77",
+         "#CC6677","#999933","#44AA99","#882255")
 
 
-plot(dens(sim_p))
-str(sim_p)
+idx <- order(colMeans(post$beta_K))
+names = c("Urinary & Male\nGenital", "Female\nGenital", "Haematolymphoid",
+          "CNS", "BST", "Endocrine and\nNeuroendocrine", "Breast", "Throacic",
+          "Gastrointestinal","Skin")
+#png('./figures/latent_knowledge.png', 
+#    width = pic.size, height = pic.size, res = 300)
+plot(NULL, ylim=c(-2,2), xlim = c(0.5,10.5),
+     xlab = '', ylab = 'Density', xaxt = 'null', 
+     main = 'Latent Knowledge in Each Field')
+abline(h = 0, lty=2)
+s_factor <- 2 # scaling factor for graphics
+for(i in 1:10) {
+  y <- density(post$beta_K[,idx[i]])$x
+  x <- density(post$beta_K[,idx[i]])$y
+  polygon(i + x/s_factor, y, col = scales::alpha(col[[i]],0.6), border = FALSE)
+  lines(i + x/s_factor, y, lwd = 1)
+  polygon(i - x/s_factor, y, col = scales::alpha(col[[i]],0.6), lwd = 2, border = FALSE)
+  lines(i - x/s_factor, y, lwd = 1)
+}
+axis(1, at = 1:10, labels = FALSE)
+text(x = 1:10, -2.3,
+     labels = names,  
+     srt=75,  adj=1,    xpd=TRUE)
+mtext("C", side=side, line=line, cex=cex, adj=adj)
 
-# inspect the differences of the different 
-# strategies at parameter level 
+#levels(as.factor(db$Area))[idx]
+# THIRD PLOT ----------------------------------------------------
+
+
+# FOURTH PLOT -------------------------------------------------
 pic.size <- 2000
 
 idx <- order(colMeans(post$gamma))
-png('./figures/prompt_density.png', 
-    width = pic.size, height = pic.size, res = 300)
+#png('./figures/prompt_density.png', 
+#    width = pic.size, height = pic.size, res = 300)
 s_factor <- 3.2 # scaling factor for graphics
-plot(N, ylim=c(-1.8,2.2), xlim = c(0.5,4.5),
+plot(NULL, ylim=c(-2.2,2.2), xlim = c(0.5,4.5),
      xlab = '', ylab = 'Density', xaxt = 'null', 
      main = 'Prompt Modality')
 abline(h = 0, lty = 2)
 for(i in 1:4) {
   y <- density(post$gamma[,idx[i]])$x
   x <- density(post$gamma[,idx[i]])$y
-  polygon(i + x/s_factor, y, col = scales::alpha(i,0.6), border = FALSE)
+  polygon(i + x/s_factor, y, col = scales::alpha(col[[i]],0.6), border = FALSE)
   lines(i + x/s_factor, y, lwd = 1)
-  polygon(i - x/s_factor, y, col = scales::alpha(i,0.6), lwd = 2, border = FALSE)
+  polygon(i - x/s_factor, y, col = scales::alpha(col[[i]],0.6), lwd = 2, border = FALSE)
   lines(i - x/s_factor, y, lwd = 1)
+  
+  
+  #vioplot::vioplot(post$gamma[,idx[i]],at=i,drawRect=FALSE,col = scales::alpha(col[[i]],0.6),add =TRUE)
+  
 }
-levels( as.factor(paste(db$type,db$reference)))[idx]
-leg_text <- c( 'OE-R',  'OE-NR', 'MC-R', 'MC-NR')
-axis(1, 1:4, leg_text)
+levels(as.factor(paste(db$type,db$reference)))[idx]
+leg_text <- c( 'Open Ended\nReference','Open Ended\nNo Reference', 
+               'Multiple Choice\nReference', 'Multiple Choice\nNo Reference')
+axis(1, at = 1:4, labels = FALSE)
+text(x = 1:4, -2.6,
+     labels = leg_text,  
+     srt=75,  adj=1,    xpd=TRUE)
+mtext("D", side=side, line=line, cex=cex, adj=adj)
+
+
+# FOURTH PLOT -------------------------------------------------
 dev.off()
-
-
 
 
 # the aim of the work is to assess the reliability 
 # in addressing pathological problems/questions
 
-idx <- order(colMeans(post$beta_K))
-
-png('./figures/latent_knowledge.png', 
-    width = pic.size, height = pic.size, res = 300)
-plot(N, ylim=c(-1.6,2), xlim = c(0.5,10.5),
-     xlab = '', ylab = 'Density', xaxt = 'null', 
-     main = 'Latent Knowledge in each field')
-abline(h = 0)
-s_factor <- 2 # scaling factor for graphics
-for(i in 1:10) {
-  y <- density(post$beta_K[,idx[i]])$x
-  x <- density(post$beta_K[,idx[i]])$y
-  polygon(i + x/s_factor, y, col = scales::alpha(i,0.6), border = FALSE)
-  lines(i + x/s_factor, y, lwd = 1)
-  polygon(i - x/s_factor, y, col = scales::alpha(i,0.6), lwd = 2, border = FALSE)
-  lines(i - x/s_factor, y, lwd = 1)
-}
-axis(1, at = 1:10, labels = FALSE)
-text(x = 1:10 - 0.1, -1.9,
-     labels = levels(as.factor(db$Area))[idx],  
-     srt=45,  adj=1,    xpd=TRUE)
-dev.off()
 
 
 
@@ -187,10 +213,11 @@ col <- c("#88CCEE","#CC6677","#DDCC77",
 field <- rep(1:10, each = 5 )
 idx <- order(colMeans(post$delta))
 y_text <- colMeans(post$delta)
-png('./figures/scenario_effect.png', 
-    width = pic.size*2, height = pic.size*1.4, res = 300)
 
-plot(N, ylim=c(-1.6,1.9), xlim = c(0.5,50.5),
+#png('./figures/scenario_effect.png', 
+#    width = pic.size*2, height = pic.size*1.4, res = 300)
+
+plot(NULL, ylim=c(-1.6,1.9), xlim = c(0.5,50.5),
      xlab = '', ylab = 'Density', xaxt = 'null', 
      main = 'Effect of scenarios')
 abline(h = 0)
@@ -207,4 +234,4 @@ for(i in 1:50) {
 lab <- c('Soft', 'Breast', 'Endocrine', 'Hemolymph', 
          'Uro', 'Gyn', 'GI', 'Thoracic', 'Skin', 'CNS')
 legend('topleft', pch = rep(16,10), col = col, legend = lab)
-dev.off()
+
